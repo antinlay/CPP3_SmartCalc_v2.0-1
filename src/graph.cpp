0946df3258cbf6f921f60a810bb286fe255d030c
Janiece Eleonor <janiecee@un-j4.kzn.21-school.ru>
2023-08-15 17:35:35 +0300

    ADD STYLE FOR GRAPH AND DRAG AND ZOOM GRAPHIC

--- src/graph.cpp
+++ src/graph.cpp
@@ -21,8 +21,19 @@ void Graph::graphClicked() {
   ui->widget->clearGraphs();
   // create Graph and assign data to it:
   ui->widget->addGraph();
-  ui->widget->setInteraction(QCP::iRangeZoom);
+  ui->widget->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom);
+  // create style
+  QCPScatterStyle scatterStyle;
+  scatterStyle.setShape(QCPScatterStyle::ssDisc); // Задаем форму точки, например, круг
+  scatterStyle.setPen(QPen(Qt::black)); // Задаем цвет контура точки
+  scatterStyle.setBrush(Qt::black); // Задаем цвет заполнения точки
+  scatterStyle.setSize(2);
+  // set only dots without line
+  ui->widget->graph(0)->setLineStyle(QCPGraph::lsNone);
+  ui->widget->graph(0)->setScatterStyle(scatterStyle);
+
   ui->widget->graph(0)->setData(x, y);
+
   // give the axes some labels:
   ui->widget->xAxis->setLabel("x");
   ui->widget->yAxis->setLabel("y");
